3b82596268e09702ea95ba2171974b87f2d44865
ManoloiuAlexandru <34137425+ManoloiuAlexandru@users.noreply.github.com>
2020-02-19 16:25:11 +0200

    Update Player.js

--- Player.js
+++ Player.js
@@ -4,8 +4,9 @@ class Player
 	{
 		this.deck=deck;
 		this.hand=NaN;
+		this.hp=20;
 	}
-	print_hand(players_turn)
+	print_hand(players_turn,battle_for_player)
 	{
 		var i;
 		var result="";
@@ -16,7 +17,7 @@ class Player
 			card_to_play.setAttribute("id","Card"+players_turn+i);
 			card_to_play.onclick=function()
 			{
-				play_it(this.id);
+				play_it(this.id,battle_for_player);
 			}
 			card_to_play.innerHTML=result;
 			card_to_play.value=result;
@@ -24,10 +25,25 @@ class Player
 		}
 	}
 }
-function play_it(id_of_card)
+function play_it(id_of_card,battle_for_player)
 {
-	var result="";
-	result=document.getElementById(id_of_card).value;
-	document.getElementById("battlefield").innerHTML=document.getElementById("battlefield").innerHTML+result;
+	var card=document.getElementById(id_of_card);
+	var card_on_field=document.createElement("Button");
+	card_on_field.setAttribute("id","B"+id_of_card);
+	card_on_field.onclick=function()
+	{
+		action(this.id,battle_for_player);
+	}
+	console.log(battle_for_player);
+	card_on_field.innerHTML=document.getElementById(id_of_card).value;
+	card_on_field.value=document.getElementById(id_of_card).value;
+	document.getElementById(battle_for_player).appendChild(card_on_field);
 	document.getElementById(id_of_card).remove();
 }
+function action(id_of_card,battle_for_player)
+{
+	console.log(id_of_card);
+	console.log(battle_for_player);
+}
+player1=new Player(deck_for_test);
+player2=new Player(deck_for_test);
