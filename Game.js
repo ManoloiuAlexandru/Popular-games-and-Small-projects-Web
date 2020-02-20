307428bd8677c0670f836345ebcfa47c858c1796
ManoloiuAlexandru <34137425+ManoloiuAlexandru@users.noreply.github.com>
2020-02-20 12:19:18 +0200

    Update Game.js

--- Game.js
+++ Game.js
@@ -1,39 +1,57 @@
-class Creature
+function end_turn(id_player)
 {
-	constructor(mana,attack,hp,name,type,list_of_attrib)
+	if (id_player=="player1")
 	{
-		this.mana=mana;
-		this.name=name;
-		this.type=type;
-		this.attack=attack;
-		this.hp=hp;
-		this.draw_card=false;
-		this.guard=false;
-		this.nobel=false;
-		this.list_of_attrib=list_of_attrib;
-		if (list_of_attrib.includes("Guard"))
-		{
-			this.guard=true;
-		}
-		if (list_of_attrib.includes("Draw a card"))
-		{
-			this.draw_card=true;
-		}
-		if (list_of_attrib.includes("Nobel"))
-		{
-			this.nobel=true;
-		}
-	}
-	
-	print_creature()
-	{
-		var result="";
-		result=this.mana+" "+this.type+"\n"+this.name+"\n"+this.attack+"   "+this.hp+"\n";
+		document.getElementById("player1").disabled =true;
+		document.getElementById("player2").disabled =false;
 		var i;
-		for (i=0;i<this.list_of_attrib.length;i++)
+		for (i=0;i<player2.creature_on_field.length;i++)
 		{
-			result=result+this.list_of_attrib[i]+",";
+			player2.creature_on_field[i].disabled=false;
+			player2.creature_can_attack.push(player2.creature_on_field[i]);
 		}
-		return result;
+		draw_a_card(player1);
+		console.log(player1);
+		console.log(player2);
+	}
+	else if (id_player=="player2")
+	{
+		document.getElementById("player2").disabled =true;
+		document.getElementById("player1").disabled =false;
+		var i;
+		for (i=0;i<player1.creature_on_field.length;i++)
+		{
+			player1.creature_on_field[i].disabled=false;
+			player1.creature_can_attack.push(player1.creature_on_field[i]);
+		}
+		draw_a_card(player2);
+		console.log(player1);
+		console.log(player2);
+	}
+}
+
+function action(id_of_card,battle_for_player)
+{
+	if (battle_for_player=="battlefield_for_player2")
+	{
+		attack_hp=document.getElementById(id_of_card).value;
+		attack_hp=attack_hp.split("\n");
+		attack_hp=attack_hp[2].split(" ");
+		attack=attack_hp[0];
+		hp=attack_hp[attack_hp.length-1];
+		player1.hp=player1.hp-attack;
+		document.getElementById("player1_hp").innerHTML="HP of player1:"+player1.hp;
+		document.getElementById(id_of_card).disabled=true;
+	}
+	else
+	{
+		attack_hp=document.getElementById(id_of_card).value;
+		attack_hp=attack_hp.split("\n");
+		attack_hp=attack_hp[2].split(" ");
+		attack=attack_hp[0]
+		hp=attack_hp[attack_hp.length-1];
+		player2.hp=player2.hp-attack;
+		document.getElementById("player2_hp").innerHTML="HP of player2:"+player2.hp;
+		document.getElementById(id_of_card).disabled=true;
 	}
 }
