d58e44473e50b22aa3ca7961ffa7eba498460b53
ManoloiuAlexandru <34137425+ManoloiuAlexandru@users.noreply.github.com>
2020-02-22 13:56:28 +0200

    Update Library.js

--- Library.js
+++ Library.js
@@ -1,15 +1,15 @@
 Creatures=
 [
 [0,1,1,"Peasant","Soldier",[]],
-//[0,999,99999,"Alex","Soldier",["Guard","Draw a card","Nobel","Rush"]],
+//[0,999,99999,"Alex","Soldier",["Guard( creature with guard must be attacked first)","Draw a card","Nobel","Rush( creature with rush can attack in the same turn)"]],
 [0,2,1,"Archer","Soldier",[]],
 [0,3,3,"Knight","Soldier",[]],
-[0,1,5,"Gate Guard","Soldier",["Guard"]],
-[0,3,1,"Cavalry","Soldier",["Rush"]],
-[0,1,1,"Scout","Soldier",["Draw a card"]],
+[0,1,5,"Gate Guard","Soldier",["Guard( creature with guard must be attacked first)"]],
+[0,3,1,"Cavalry","Soldier",["Rush( creature with rush can attack in the same turn)"]],
+/*[0,1,1,"Scout","Soldier",["Draw a card"]],
 [0,2,3,"Paladin","Soldier",["When a friendly soldier dies get+1/+1"]],
 [0,3,8,"Royal Guard","Soldier",["Guard","Draw a card"]],
-[0,3,3,"Knight Champion","Soldier",["When you summon a soldier get +1/+1"]],
+[0,3,3,"Knight Champion","Soldier",["When you summon a soldier get +1/+1"]],*/
 ]
 
 var deck_for_test_player1=[];
