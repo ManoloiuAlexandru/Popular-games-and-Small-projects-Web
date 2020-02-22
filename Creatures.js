afe66667ed0872643b81dc4f88af3912dbcee180
ManoloiuAlexandru <34137425+ManoloiuAlexandru@users.noreply.github.com>
2020-02-22 13:54:15 +0200

    Update Creatures.js

--- Creatures.js
+++ Creatures.js
@@ -7,22 +7,7 @@ class Creature
 		this.type=type;
 		this.attack=attack;
 		this.hp=hp;
-		this.draw_card=false;
-		this.guard=false;
-		this.nobel=false;
 		this.list_of_attrib=list_of_attrib;
-		if (list_of_attrib.includes("Guard"))
-		{
-			this.guard=true;
-		}
-		if (list_of_attrib.includes("Draw a card"))
-		{
-			this.draw_card=true;
-		}
-		if (list_of_attrib.includes("Nobel"))
-		{
-			this.nobel=true;
-		}
 	}
 	
 	print_creature()
@@ -34,6 +19,12 @@ class Creature
 		{
 			result=result+this.list_of_attrib[i]+",";
 		}
+		return result;
+	}
+	print_non_attrib()
+	{
+		var result="";
+		result=this.mana+" "+this.type+"\n"+this.name+"\n"+this.attack+"   "+this.hp+"\n";
 		return result;
 	}
 }
