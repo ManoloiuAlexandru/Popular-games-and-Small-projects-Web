d8cbf712e57943370511f7556430786d51c77ff7
ManoloiuAlexandru <34137425+ManoloiuAlexandru@users.noreply.github.com>
2020-02-26 20:24:05 +0200

    Update Graphics_and_art_style.js

--- Graphics_and_art_style.js
+++ Graphics_and_art_style.js
@@ -3,4 +3,5 @@ function Style_cards(card_to_style)
 	card_to_style.style.height='120px';
 	card_to_style.style.width='60px';
 	card_to_style.style.fontSize='13.2px';
+	card_to_style.style.margin='10px';
 }
