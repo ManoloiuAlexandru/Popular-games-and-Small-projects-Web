9273eb905950beb0342f74be5492b83bf268c0d1
ManoloiuAlexandru <34137425+ManoloiuAlexandru@users.noreply.github.com>
2020-03-03 15:50:23 +0200

    Update server.py

--- server.py
+++ server.py
@@ -12,18 +12,49 @@ pusher = pusher_client = pusher.Pusher(
 )
 
 name = ''
+users_playing = list()
+user_picked = list()
+picking_player = ''
+picked_player = ''
 
 
 @app.route('/')
 def index():
+    print(users_playing)
     return render_template('index.html')
 
 
 @app.route('/play')
 def play():
     global name
+    global users_playing
+    global user_picked
     name = request.args.get('username')
+    email = request.args.get('email')
+
+    if name is None:
+        name = ''
+
+    if email is None:
+        emial = name
+
+    users_playing.append("/play?username=" + name)
+    print(picking_player)
+    print(picked_player)
     return render_template('play.html')
+
+
+@app.route('/picked', methods=['GET'])
+def picked():
+    global picked_player
+    global picking_player
+    picking_player = json.dumps(name)
+    player_picked = request.args.get('fname')
+    for i in users_playing:
+        if player_picked in i:
+            picked_player = i
+            return render_template('arena.html')
+    return json.dumps("That player is not online")
 
 
 @app.route("/pusher/auth", methods=['POST'])
@@ -41,7 +72,9 @@ def pusher_authentication():
     return json.dumps(auth)
 
 
+global player1
+global player2
+
 if __name__ == '__main__':
     app.run(host='0.0.0.0', debug=True)
-
 name = ''
